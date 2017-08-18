5d531b5e90002208a4b809ef28d0a2c928ffc25e
Lei Mingyu <uygnim.iel@gmail.com>
2017-08-18 13:56:37 +0800

    initialization of project

---
+++ 🌚 🌈🔫🌝/🌚 🌈🔫🌝/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  🌚 🌈🔫🌝
+//
+//  Created by Lei Mingyu on 18/8/17.
+//  Copyright © 2017 MFRG. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 🌚 🌈🔫🌝/🌚 🌈🔫🌝/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  🌚 🌈🔫🌝
+//
+//  Created by Lei Mingyu on 18/8/17.
+//  Copyright © 2017 MFRG. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
