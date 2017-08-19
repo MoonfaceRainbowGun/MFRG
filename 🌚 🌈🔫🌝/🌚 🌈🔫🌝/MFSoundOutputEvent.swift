63f23bbcf726c4f88df2454a929911f8250cda64
Lei Mingyu <uygnim.iel@gmail.com>
2017-08-19 10:45:23 +0800

    add scale

--- 🌚 🌈🔫🌝/🌚 🌈🔫🌝/MFSoundOutputEvent.swift
+++ 🌚 🌈🔫🌝/🌚 🌈🔫🌝/MFSoundOutputEvent.swift
@@ -14,13 +14,14 @@ class MFSoundOutputEvent: MFOutputEvent {
     fileprivate var playerDicts = [String: MFSoundPlayer]()
     
     override func run(_ userInfo: [String: Any]) {
-        if let fileName = userInfo["FileName"] as? String {
+//        if let fileName = userInfo["FileName"] as? String {
+        let fileName = "D.mp3"
             if playerDicts[fileName] == nil {
                 playerDicts[fileName] = MFSoundPlayer(sound: fileName)
             }
             let player = playerDicts[fileName]!
             player.play()
-        }
+//        }
     }
     
     override class func defaultInstance() -> MFOutputEvent {
